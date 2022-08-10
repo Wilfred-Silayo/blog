fda157f4accdaee0f01911832019e5b0abb48925
Wilfred Silayo <wilfredsilayo99@gmail.com>
2022-08-10 16:02:28 +0300

    third commit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,13 +1,88 @@
 @extends('layouts.app')
 @section('title',' | My Profile')
+
 @section('content')
 <div class="container">
-        <div class="row justify-content-center">
-                <div class="col-md-8">
-                <div class="card">
-                <p> Profiles</p>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Update Profile') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="profile-picture" class="col-md-4 col-form-label text-md-end">{{ __('Profile Picture') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="file" type="file" class="form-control" name="profile_picture">
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Update
+                                </button>
+                                <button type="submit" class="mx-4 btn btn-secondary">
+                                    <a href="{{ __('home') }}" style="text-decoration:none; color:white;">Cancel<a>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                </div>
+            </div>
         </div>
-</div>     
- @endsection
+    </div>
+</div>
+@endsection
